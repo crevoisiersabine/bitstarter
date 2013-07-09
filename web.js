342ef2eb12571e1bb946bba59939d3d3d7328f19
Ubuntu <ubuntu@ip-172-31-32-39.us-west-2.compute.internal>
2013-07-09 21:18:15 +0000

    Pushing from disk - try 5

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-//app.get('/', function(request, response) {
-  //response.send('Hello World from index.html');
-//});
+app.get('/', function(request, response) {
+  response.send(fs.readFileSync('index.html'));
+});
 
-fs.writeFileSync('index.html');
+//fs.readFileSync('index.html');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
